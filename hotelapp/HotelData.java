ad30e44037b0d925eb4a57e0d69c11b4cf650e73
Momo0420 <130511161+Momo0420@users.noreply.github.com>
2025-12-05 15:54:56 +0000

    Add files via upload

---
+++ hotelapp/HotelData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotelapp;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author EmoshiogweOmokhudu
+ */
+public class HotelData {
+    //Stores all the room objects one shared list.
+    public static ArrayList<Room> rooms = new ArrayList<>();
+    
+}
